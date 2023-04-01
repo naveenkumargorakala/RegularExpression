baaae11d561d3084ca5affa60c3e7e1ad38182d9
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-04-01 18:25:45 +0530

    Added: Last Name is Added

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,12 +4,22 @@ import java.util.Scanner;
 public class UserRegistration {
     Scanner input = new Scanner(System.in);
     public void firstName(){
-        Pattern pattern = Pattern.compile("^[A-Z][A-Za-z]*");
+        Pattern pattern = Pattern.compile("^[A-Z][A-Za-z]{2}[A-Za-z]*");
         System.out.println("Enter Name:  ");
         String FirstName= input.next();
         Matcher matcher = pattern.matcher(FirstName);
         if(matcher.matches())
             System.out.println("FirstName is: "+FirstName);
+        else
+            System.out.println("Invalid");
+    }
+    public void lastName(){
+        Pattern pattern = Pattern.compile("^[A-Z][A-Za-z]{2}[A-Za-z]*");
+        System.out.println("Enter Name:  ");
+        String lastName= input.next();
+        Matcher matcher = pattern.matcher(lastName);
+        if(matcher.matches())
+            System.out.println("lastName is: "+lastName);
         else
             System.out.println("Invalid");
     }
